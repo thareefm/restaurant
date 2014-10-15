55cbd4a2155aa52b58f63d92cc9bc089ccc51f73
thareefm <thareef.m@gmail.com>
2014-10-14 22:25:58 -0400

    Finished CRUD functionality for Food Class

--- app.rb
+++ app.rb
@@ -3,12 +3,61 @@ Bundler.require
 
 ActiveRecord::Base.establish_connection({
 	adapter: 'postgresql',
-	database: 'restaurant-db'
+	database: 'restaurant_db'
 	})
 
 ROOT_PATH = Dir.pwd
 Dir[ROOT_PATH+"/models/*.rb"].each{ |file| require file }
 
 get '/' do
-	"hello"
+	redirect '/foods'
+end	
+
+
+# index: Display a list of food items available
+get '/foods' do
+	@foods = Food.all
+	erb :'foods/index'
 end
+
+# Display a form for a new food item
+get '/foods/new' do
+	erb :'foods/new'
+end
+
+# Creates a new food item
+post '/foods' do
+	Food.create(params[:foods])
+	redirect '/foods'
+end
+
+# Display a form to edit a food item
+get '/foods/:id/edit' do
+	@foods = Food.find(params[:id])
+	erb :'foods/edit'
+end
+
+# Updates a food item
+patch '/foods/:id' do
+	@foods = Food.find(params[:id])
+	@foods.update(params[:foods])
+	redirect "/foods/#{@foods.id}"
+end
+
+#Display a single food item and a list of all the parties that included it
+get '/foods/:id' do
+	@foods = Food.find(params[:id])
+	# @parties = @foods.parties
+	erb :'foods/show'
+end
+
+# Deletes a food item
+delete '/foods/:id' do
+	Food.delete(params[:id])
+	redirect '/foods'
+end
+
+
+
+
+
---
+++ restuarant.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE restaurant_db;
+\c restaurant_db
+
+CREATE TABLE foods (
+id SERIAL PRIMARY KEY,
+name varchar(150),
+cuisine_type varchar(100),
+calories integer,
+price integer
+);
+
+CREATE TABLE parties (
+id SERIAL PRIMARY KEY,
+table_number integer,
+number_guests integer,
+pay_status boolean,
+order_id integer
+);
+
+CREATE TABLE orders (
+id SERIAL PRIMARY KEY,
+food_id integer,
+party_id integer
+);
+
+\q
